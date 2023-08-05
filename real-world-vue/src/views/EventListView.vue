654c6f2d722d008c7abbc12e534f8c9668d80fc6
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-05 14:13:14 +0700

    Finish Lab 2

--- real-world-vue/src/views/EventListView.vue
+++ real-world-vue/src/views/EventListView.vue
@@ -1,25 +1,26 @@
 <script setup lang="ts">
 import EventCard from '../components/EventCard.vue'
 import type { EventItem } from '@/type'
-import { ref, type Ref } from 'vue'
-import EventService from '@/services/EventService'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import eventService from '@/services/EventService'
+ 
 const events: Ref<Array<EventItem>> = ref([])
-EventService.getEvent().then((response) => {  
-  events.value = response.data
-})
+  eventService.getEvent().then((response) => {
+    events.value = response.data
+  })
+  
 </script>
 
 <template>
-  <h1>Events For Good</h1> 
   <main class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
   </main>
 </template>
 
 <style scoped>
-.events {
+.events{
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-</style>
+}</style>
--- real-world-vue/src/views/StudentView.vue
+++ real-world-vue/src/views/StudentView.vue
@@ -1,19 +1,24 @@
 <template>
-    <h1>Events For Good</h1>
-    <main class="events">
-      <StudentCard v-for="student in students" :student="student" :key="student.id"></StudentCard>
-    </main>
-  </template>
-  
-  <script setup lang="ts">
-  import type { StudentItem } from '@/type'
-  import StudentCard from '@/components/StudentCard.vue'
-  import { ref, type Ref } from 'vue'
-  import StuedntService from '@/services/StudentService'
-  const students: Ref<Array<StudentItem>> = ref([])
-  StuedntService.getEvent().then((res) => {
-    students.value = res.data
-  })
-  </script>
-  
-  <style scoped></style>
+  <main class="events">
+    <StudentCard v-for="student in students" :student="student" :key="student.id"></StudentCard>
+  </main>
+</template>
+
+<script setup lang="ts">
+import type { StudentItem } from '@/type'
+import StudentCard from '@/components/StudentCard.vue'
+import { ref, type Ref } from 'vue'
+import StuedntService from '@/services/StudentService'
+const students: Ref<Array<StudentItem>> = ref([])
+StuedntService.getEvent().then((response) => {
+  students.value = response.data
+})
+</script>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
